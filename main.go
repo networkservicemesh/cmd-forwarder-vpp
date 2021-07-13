ce6e9f0f66c6e0fa09d5ea372c6e593ef2233a7b
Denis Tingaikin <49399980+denis-tingaikin@users.noreply.github.com>
2021-07-14 05:46:55 +0700

    add spire certs updating (#271)
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/tools/log"
 	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
 	"github.com/networkservicemesh/sdk/pkg/tools/opentracing"
+	"github.com/networkservicemesh/sdk/pkg/tools/resetting"
 	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
 	"github.com/networkservicemesh/sdk/pkg/tools/token"
 
@@ -173,7 +174,10 @@ func main() {
 		&config.ConnectTo,
 		vppConn,
 		vppinit.Must(config.VppInit.Execute(ctx, vppConn, config.TunnelIP)),
-		grpc.WithTransportCredentials(grpcfd.TransportCredentials(credentials.NewTLS(tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny())))),
+		grpc.WithTransportCredentials(
+			resetting.NewCredentials(
+				grpcfd.TransportCredentials(credentials.NewTLS(tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()))),
+				source.Updated())),
 		grpc.WithDefaultCallOptions(
 			grpc.WaitForReady(true),
 			grpc.PerRPCCredentials(token.NewPerRPCCredentials(spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime))),
