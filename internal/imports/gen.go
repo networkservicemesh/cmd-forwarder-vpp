97274f7658c4185a6a3a35c4b33712d6f96ef317
Ed Warnicke <hagbard@gmail.com>
2022-05-15 13:57:59 -0500

    Set minumum TLS version to 1.2
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- internal/imports/gen.go
+++ internal/imports/gen.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Cisco and/or its affiliates.
+// Copyright (c) 2020-2022 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -17,5 +17,5 @@
 package imports
 
 //go:generate bash -c "rm -f imports*.go"
-//go:generate bash -c "cd $(mktemp -d) && GO111MODULE=on go get github.com/edwarnicke/imports-gen@v1.1.2"
+//go:generate bash -c "cd $(mktemp -d) && GO111MODULE=on go install github.com/edwarnicke/imports-gen@v1.1.2"
 //go:generate bash -c "GOOS=linux ${GOPATH}/bin/imports-gen"
