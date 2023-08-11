e1dd0133b8db759134f40a68dad34c81f32f6e5a
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-08-11 19:57:46 +0700

    Delete grpc.WithBlock() and grpc.WaitForReady(true) options
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- internal/tests/suite_healthcheck_test.go
+++ internal/tests/suite_healthcheck_test.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/networkservicemesh/sdk/pkg/tools/log"
@@ -38,7 +37,6 @@ func (f *ForwarderTestSuite) TestHealthCheck() {
 		&grpc_health_v1.HealthCheckRequest{
 			Service: "networkservice.NetworkService",
 		},
-		grpc.WaitForReady(true),
 	)
 	f.NoError(err)
 	f.Require().NotNil(healthResponse)
